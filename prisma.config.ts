d12f7a5761c373961cafd4b245e0337bc2251e73
Abhishek Maurya <AbhishekKumar.Maurya@ghd.com>
2026-04-30 03:00:00 +0530

    chore: update hardcoded database password

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: "postgresql://postgres.ngivaozrabxyyyxqfdbx:%40BHI2f24@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?pgbouncer=true",
+    url: "postgresql://postgres.ngivaozrabxyyyxqfdbx:EwxZpz%3FM-PL6Kpy@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?pgbouncer=true",
   },
 });
